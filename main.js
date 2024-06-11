01cda17db9d9ee485f2d4e1f0bcca1d08409cd8d
Shaswata <shaswatapanda3@gmail.com>
2024-06-11 12:28:32 +0530

    This is good

--- main.js
+++ main.js
@@ -1,16 +1,17 @@
 let name = {
     fname: "Anjali",
     Lname: "Bana",
-    A: function() {
-        console.log(this.fname + " " + this.Lname);
-    }
+   
 };
-
-name.A(); // Output: Anjali Bana
+let a= function() {
+    console.log(this.fname + " " +this. Lname);
+}
+ // Output: Anjali Bana
 
 let name2 = {
     fname: "KATI",
     Lname: "Banda"
 };
 
-name.A.call(name2); // Output: KATI Banda
+a.call(name2); // Output: KATI Banda
+a.call(name);
